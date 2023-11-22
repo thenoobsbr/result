3b1414ccddc96ce1f14c5a59a9112bd7a8bbc608
Tiago Resende <tiagor87@gmail.com>
2023-11-21 21:38:43 -0300

    :art: Improve code

--- src/TheNoobs.Results/Exceptions/InvalidResultValueException.cs
+++ src/TheNoobs.Results/Exceptions/InvalidResultValueException.cs
@@ -5,14 +5,14 @@ using System.Runtime.Serialization;
 namespace TheNoobs.Results.Exceptions;
 
 [Serializable]
-public class InvalidResultValueException : Exception
+public sealed class InvalidResultValueException : Exception
 {
     public InvalidResultValueException() : base("The result is not success, so it does not have a value.")
     {
     }
 
     #if NET6_0 || NET7_0
-    public InvalidResultValueException(SerializationInfo info, StreamingContext context) : base(info, context)
+    private InvalidResultValueException(SerializationInfo info, StreamingContext context) : base(info, context)
     {
     }
     #endif
