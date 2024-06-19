ee6165414921e88bc39a02783ca5e61787c03c8b
Tiago C. Resende <tiagor87@gmail.com>
2024-06-18 21:08:49 -0300

    :sparkles: Implements merge method

---
+++ src/TheNoobs.Results/Extensions/ResultExtensionsMerge.cs
@@ -0,0 +1,144 @@
+﻿namespace TheNoobs.Results.Extensions;
+
+public static partial class ResultExtensions
+{
+    public static Result<(T1, T2)> Merge<T1, T2>(this Result<T1> result, Result<T2> other)
+    {
+        if (!result.IsSuccess)
+        {
+            return result.Fail!;
+        }
+        
+        if (!other.IsSuccess)
+        {
+            return other.Fail!;
+        }
+        
+        return new Result<(T1, T2)>((
+            result.Value,
+            other.Value));
+    }
+    
+    public static Result<(T1, T2, T3)> Merge<T1, T2, T3>(this Result<(T1, T2)> result, Result<T3> other)
+    {
+        if (!result.IsSuccess)
+        {
+            return result.Fail!;
+        }
+        
+        if (!other.IsSuccess)
+        {
+            return other.Fail!;
+        }
+        
+        return new Result<(T1, T2, T3)>((
+            result.Value.Item1,
+            result.Value.Item2,
+            other.Value));
+    }
+    
+    public static Result<(T1, T2, T3, T4)> Merge<T1, T2, T3, T4>(this Result<(T1, T2, T3)> result, Result<T4> other)
+    {
+        if (!result.IsSuccess)
+        {
+            return result.Fail!;
+        }
+        
+        if (!other.IsSuccess)
+        {
+            return other.Fail!;
+        }
+        
+        return new Result<(T1, T2, T3, T4)>((
+            result.Value.Item1,
+            result.Value.Item2,
+            result.Value.Item3,
+            other.Value));
+    }
+    
+    public static Result<(T1, T2, T3, T4, T5)> Merge<T1, T2, T3, T4, T5>(this Result<(T1, T2, T3, T4)> result, Result<T5> other)
+    {
+        if (!result.IsSuccess)
+        {
+            return result.Fail!;
+        }
+        
+        if (!other.IsSuccess)
+        {
+            return other.Fail!;
+        }
+        
+        return new Result<(T1, T2, T3, T4, T5)>((
+            result.Value.Item1,
+            result.Value.Item2,
+            result.Value.Item3,
+            result.Value.Item4,
+            other.Value));
+    }
+    
+    public static Result<(T1, T2, T3, T4, T5, T6)> Merge<T1, T2, T3, T4, T5, T6>(this Result<(T1, T2, T3, T4, T5)> result, Result<T6> other)
+    {
+        if (!result.IsSuccess)
+        {
+            return result.Fail!;
+        }
+        
+        if (!other.IsSuccess)
+        {
+            return other.Fail!;
+        }
+        
+        return new Result<(T1, T2, T3, T4, T5, T6)>((
+            result.Value.Item1,
+            result.Value.Item2,
+            result.Value.Item3,
+            result.Value.Item4,
+            result.Value.Item5,
+            other.Value));
+    }
+    
+    public static Result<(T1, T2, T3, T4, T5, T6, T7)> Merge<T1, T2, T3, T4, T5, T6, T7>(this Result<(T1, T2, T3, T4, T5, T6)> result, Result<T7> other)
+    {
+        if (!result.IsSuccess)
+        {
+            return result.Fail!;
+        }
+        
+        if (!other.IsSuccess)
+        {
+            return other.Fail!;
+        }
+        
+        return new Result<(T1, T2, T3, T4, T5, T6, T7)>((
+            result.Value.Item1,
+            result.Value.Item2,
+            result.Value.Item3,
+            result.Value.Item4,
+            result.Value.Item5,
+            result.Value.Item6,
+            other.Value));
+    }
+    
+    public static Result<(T1, T2, T3, T4, T5, T6, T7, T8)> Merge<T1, T2, T3, T4, T5, T6, T7, T8>(this Result<(T1, T2, T3, T4, T5, T6, T7)> result, Result<T8> other)
+    {
+        if (!result.IsSuccess)
+        {
+            return result.Fail!;
+        }
+        
+        if (!other.IsSuccess)
+        {
+            return other.Fail!;
+        }
+        
+        return new Result<(T1, T2, T3, T4, T5, T6, T7, T8)>((
+            result.Value.Item1,
+            result.Value.Item2,
+            result.Value.Item3,
+            result.Value.Item4,
+            result.Value.Item5,
+            result.Value.Item6,
+            result.Value.Item7,
+            other.Value));
+    }
+}
---
+++ tests/TheNoobs.Results.Tests/ResultExtensionsMergeTests.cs
@@ -0,0 +1,228 @@
+﻿using FluentAssertions;
+using TheNoobs.Results.Extensions;
+using TheNoobs.Results.Tests.Stubs;
+
+namespace TheNoobs.Results.Tests;
+
+public class ResultExtensionsMergeTests
+{
+    [Fact]
+    public void GivenResultWhenSuccessThenMergeOneValueShouldReturnTheValue()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        result.Merge(result2).Value.Should().Be((1, 2));
+    }
+    
+    [Fact]
+    public void GivenResultWhenFailThenMergeOneValueShouldReturnTheFail()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(new TestFail());
+        result.Merge(result2).Fail.Should().BeOfType<TestFail>();
+    }
+    
+    [Fact]
+    public void GivenResultWhenSuccessThenMergeTwoValueShouldReturnTheValue()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Value.Should().Be((1, 2, 3));
+    }
+    
+    [Fact]
+    public void GivenResultWhenFailThenMergeTwoValueShouldReturnTheFail()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(new TestFail());
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Fail.Should().BeOfType<TestFail>();
+    }
+    
+    [Fact]
+    public void GivenResultWhenSuccessThenMergeThreeValueShouldReturnTheValue()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Value.Should().Be((1, 2, 3, 4));
+    }
+    
+    [Fact]
+    public void GivenResultWhenFailThenMergeThreeValueShouldReturnTheFail()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(new TestFail());
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Fail.Should().BeOfType<TestFail>();
+    }
+    
+    [Fact]
+    public void GivenResultWhenSuccessThenMergeFourValueShouldReturnTheValue()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(5);
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Value.Should().Be((1, 2, 3, 4, 5));
+    }
+    
+    [Fact]
+    public void GivenResultWhenFailThenMergeFourValueShouldReturnTheFail()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(new TestFail());
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Fail.Should().BeOfType<TestFail>();
+    }
+    
+    [Fact]
+    public void GivenResultWhenSuccessThenMergeFiveValueShouldReturnTheValue()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(5);
+        var result6 = new Result<int>(6);
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Merge(result6)
+            .Value.Should().Be((1, 2, 3, 4, 5, 6));
+    }
+    
+    [Fact]
+    public void GivenResultWhenFailThenMergeFiveValueShouldReturnTheFail()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(5);
+        var result6 = new Result<int>(new TestFail());
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Merge(result6)
+            .Fail.Should().BeOfType<TestFail>();
+    }
+    
+    [Fact]
+    public void GivenResultWhenSuccessThenMergeSixValueShouldReturnTheValue()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(5);
+        var result6 = new Result<int>(6);
+        var result7 = new Result<int>(7);
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Merge(result6)
+            .Merge(result7)
+            .Value.Should().Be((1, 2, 3, 4, 5, 6, 7));
+    }
+    
+    [Fact]
+    public void GivenResultWhenFailThenMergeSixValueShouldReturnTheFail()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(5);
+        var result6 = new Result<int>(6);
+        var result7 = new Result<int>(new TestFail());
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Merge(result6)
+            .Merge(result7)
+            .Fail.Should().BeOfType<TestFail>();
+    }
+    
+    [Fact]
+    public void GivenResultWhenSuccessThenMergeSevenValueShouldReturnTheValue()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(5);
+        var result6 = new Result<int>(6);
+        var result7 = new Result<int>(7);
+        var result8 = new Result<int>(8);
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Merge(result6)
+            .Merge(result7)
+            .Merge(result8)
+            .Value.Should().Be((1, 2, 3, 4, 5, 6, 7, 8));
+    }
+    
+    [Fact]
+    public void GivenResultWhenFailThenMergeSevenValueShouldReturnTheFail()
+    {
+        var result = new Result<int>(1);
+        var result2 = new Result<int>(2);
+        var result3 = new Result<int>(3);
+        var result4 = new Result<int>(4);
+        var result5 = new Result<int>(5);
+        var result6 = new Result<int>(6);
+        var result7 = new Result<int>(7);
+        var result8 = new Result<int>(new TestFail());
+        result
+            .Merge(result2)
+            .Merge(result3)
+            .Merge(result4)
+            .Merge(result5)
+            .Merge(result6)
+            .Merge(result7)
+            .Merge(result8)
+            .Fail.Should().BeOfType<TestFail>();
+    }
+}
