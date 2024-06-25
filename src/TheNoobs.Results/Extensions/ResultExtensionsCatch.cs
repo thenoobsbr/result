081feb6786e0af43f68a702f7f8ec56b467dcdf4
Tiago C. Resende <tiagor87@gmail.com>
2024-06-25 15:48:56 -0300

    :sparkles: Implements catch

---
+++ src/TheNoobs.Results/Extensions/ResultExtensionsCatch.cs
@@ -0,0 +1,55 @@
+using TheNoobs.Results.Abstractions;
+
+namespace TheNoobs.Results.Extensions;
+
+public static partial class ResultExtensions
+{
+    public static Result<T> Catch<T, TFail>(this Result<T> result, Func<TFail, Result<T>> fail)
+        where TFail : Fail
+    {
+        if (result.IsSuccess)
+        {
+            return result;
+        }
+        
+        if (result.Fail is TFail failResult)
+        {
+            return fail(failResult);
+        }
+
+        return result;
+    }
+    
+    public static async ValueTask<Result<T>> CatchAsync<T, TFail>(this Result<T> result, Func<TFail, ValueTask<Result<T>>> failAsync)
+        where TFail : Fail
+    {
+        if (result.IsSuccess)
+        {
+            return result;
+        }
+        
+        if (result.Fail is TFail failResult)
+        {
+            return await failAsync(failResult).ConfigureAwait(false);
+        }
+
+        return result;
+    }
+    
+    public static async ValueTask<Result<T>> CatchAsync<T, TFail>(this ValueTask<Result<T>> resultAsync, Func<TFail, Result<T>> fail)
+        where TFail : Fail
+    {
+        var result = await resultAsync.ConfigureAwait(false);
+        if (result.IsSuccess)
+        {
+            return result;
+        }
+        
+        if (result.Fail is TFail failResult)
+        {
+            return fail(failResult);
+        }
+
+        return result;
+    }
+}
