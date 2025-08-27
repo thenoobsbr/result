316441dc21f51eb57daf176080ab63cf9b2aed82
Tiago Resende <tiagor87@gmail.com>
2025-08-26 21:30:25 -0300

    :art: improve naming

--- src/TheNoobs.Results/Types/Void.cs
+++ src/TheNoobs.Results/Types/Void.cs
@@ -2,7 +2,7 @@
 
 public readonly struct Void
 {
-    public static Void New => new();
+    public static Void Value => new();
     public static implicit operator Result<Void>(Void value) => new(value);
     public static implicit operator ValueTask<Result<Void>>(Void value) => ValueTask.FromResult(new Result<Void>(value));
     public static implicit operator Task<Result<Void>>(Void value) => Task.FromResult(new Result<Void>(value));
