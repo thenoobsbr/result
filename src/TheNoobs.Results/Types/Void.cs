f50d36c9f73cd78acd6d8216a76663cf309ec510
Tiago Resende <tiagor87@gmail.com>
2025-08-28 20:35:36 -0300

    :bug: fix void implicit operator error

--- src/TheNoobs.Results/Types/Void.cs
+++ src/TheNoobs.Results/Types/Void.cs
@@ -3,7 +3,6 @@
 public readonly struct Void
 {
     public static Void Value => new();
-    public static implicit operator Result<Void>(Void value) => new(value);
     public static implicit operator ValueTask<Result<Void>>(Void value) => ValueTask.FromResult(new Result<Void>(value));
     public static implicit operator Task<Result<Void>>(Void value) => Task.FromResult(new Result<Void>(value));
 }
