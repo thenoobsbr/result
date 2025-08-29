d52e27bc4af13e215b34b2e88131fcf43d5e61c9
Tiago Resende <tiagor87@gmail.com>
2025-08-28 21:01:18 -0300

    :bug: fix void return when async

--- src/TheNoobs.Results/Types/Void.cs
+++ src/TheNoobs.Results/Types/Void.cs
@@ -4,5 +4,4 @@ public readonly struct Void
 {
     public static Void Value => new();
     public static implicit operator ValueTask<Result<Void>>(Void value) => ValueTask.FromResult(new Result<Void>(value));
-    public static implicit operator Task<Result<Void>>(Void value) => Task.FromResult(new Result<Void>(value));
 }
